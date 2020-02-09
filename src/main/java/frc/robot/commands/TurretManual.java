cceda3c23127780a891d0651d99cf2da08051a1e
Alan Zhu <alanzhu2002@gmail.com>
2020-02-08 19:32:10 -0500

    Create manual Turret control command

---
+++ src/main/java/frc/robot/commands/TurretManual.java
@@ -0,0 +1,48 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.*;
+
+public class TurretManual extends CommandBase {
+private Turret turret;
+private double turretSpeed = 0.3;
+  
+  /**
+   * Creates a new TurretManual.
+   */
+  public TurretManual(Turret turret) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.turret = turret;
+    addRequirements(turret);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    turret.setTurret(turretSpeed);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    turret.stopTurret();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
