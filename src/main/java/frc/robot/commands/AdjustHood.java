459e7bee2c2619436207dac6e903aa115de158ba
Amon Green <drwhorx@gmail.com>
2020-02-19 17:57:51 -0500

    Negate the Hood Adjust calculated speed

--- src/main/java/frc/robot/commands/AdjustHood.java
+++ src/main/java/frc/robot/commands/AdjustHood.java
@@ -26,7 +26,7 @@ public class AdjustHood extends CommandBase {
      */
     @Override
     public void execute() {
-        shooter.setHood(shooter.hoodControllerCalculate());
+        shooter.setHood(-shooter.hoodControllerCalculate());
     }
 
     /**
