bf275102f43032a0d1532f781c70a4cfedf8450a
Amon Green <drwhorx@gmail.com>
2020-02-20 15:32:39 -0500

    Create Shooter velocity Shuffleboard Widget

--- src/main/java/frc/robot/commands/ShootManual.java
+++ src/main/java/frc/robot/commands/ShootManual.java
@@ -35,6 +35,7 @@ public class ShootManual extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    ShuffleboardHelpers.setWidgetValue("Shooter", "Shooter Velocity", shooter.getShooterVelocity());
   }
 
   // Called once the command ends or is interrupted.
