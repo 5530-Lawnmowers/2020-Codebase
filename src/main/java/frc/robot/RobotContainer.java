4ccd7062bc49680de9edc9e2c747a962b49f028f
Amon Green <drwhorx@gmail.com>
2020-02-20 14:51:28 -0500

    Move Hood Subsystem to Subsystem folder

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,7 +12,6 @@ import frc.robot.subsystems.*;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
--- src/main/java/frc/robot/commands/HoodAdjust.java
+++ src/main/java/frc/robot/commands/HoodAdjust.java
@@ -1,7 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Hood;
 import frc.robot.helpers.LimelightHelper;
 import frc.robot.helpers.ShuffleboardHelpers;
 
